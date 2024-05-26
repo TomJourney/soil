df00de91c6a863c1f23d95320bb3dfcece610899
TomJourney <pacosongreen@gmail.com>
2024-05-26 16:55:29 +0800

    修改maven依赖

---
+++ soil-core/src/test/java/SoilCoreTest01.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author TomBrother
+ * @version 1.0.0
+ * @ClassName SoilCoreTest01.java
+ * @Description TODO
+ * @createTime 2024年05月26日
+ */
+
+@SpringBootTest
+public class SoilCoreTest01 {
+
+    @Test
+    public void contextLoad() {
+
+    }
+
+}
